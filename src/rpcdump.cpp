4ddeb6516f68d7fb164552edf274909e803770f3
Magicone111 <42521441+Magicone111@users.noreply.github.com>
2021-01-15 20:46:46 -0800

    Update rpcdump.cpp

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -109,7 +109,7 @@ Value importprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 2)
         throw runtime_error(
-            "importprivkey <VADEprivkey> [label]\n"
+            "importprivkey <Corgicoinprivkey> [label]\n"
             "Adds a private key (as returned by dumpprivkey) to your wallet.");
 
     string strSecret = params[0].get_str();
@@ -245,8 +245,8 @@ Value dumpprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 1)
         throw runtime_error(
-            "dumpprivkey <VADEaddress>\n"
-            "Reveals the private key corresponding to <VADEnaddress>.");
+            "dumpprivkey <Corgicoinaddress>\n"
+            "Reveals the private key corresponding to <Corgicoinnaddress>.");
 
     EnsureWalletIsUnlocked();
 
@@ -297,7 +297,7 @@ Value dumpwallet(const Array& params, bool fHelp)
     std::sort(vKeyBirth.begin(), vKeyBirth.end());
 
     // produce output
-    file << strprintf("# Wallet dump created by VADE %s (%s)\n", CLIENT_BUILD.c_str(), CLIENT_DATE.c_str());
+    file << strprintf("# Wallet dump created by Corgicoin %s (%s)\n", CLIENT_BUILD.c_str(), CLIENT_DATE.c_str());
     file << strprintf("# * Created on %s\n", EncodeDumpTime(GetTime()).c_str());
     file << strprintf("# * Best block at time of backup was %i (%s),\n", nBestHeight, hashBestChain.ToString().c_str());
     file << strprintf("#   mined on %s\n", EncodeDumpTime(pindexBest->nTime).c_str());
