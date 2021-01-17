ceb1822afa43c41264c47d4d1f37f118d4faef39
Magicone111 <42521441+Magicone111@users.noreply.github.com>
2021-01-16 21:59:20 -0800

    Update wallet.h

--- src/wallet.h
+++ src/wallet.h
@@ -211,8 +211,8 @@ public:
     bool GetStakeWeight(const CKeyStore& keystore, uint64_t& nMinWeight, uint64_t& nMaxWeight, uint64_t& nWeight);
     bool CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int64_t nSearchInterval, int64_t nFees, CTransaction& txNew, CKey& key);
 
-    std::string SendMoney(CScript scriptPubKey, int64_t nValue, CWalletTx& wtxNew, bool fAskFee=false;
-    std::string SendMoneyToDestination(const CTxDestination& address, int64_t nValue, CWalletTx& wtxNew, bool fAskFee=false;
+    std::string SendMoney(CScript scriptPubKey, int64_t nValue, CWalletTx& wtxNew, bool fAskFee=false;)
+    std::string SendMoneyToDestination(const CTxDestination& address, int64_t nValue, CWalletTx& wtxNew, bool fAskFee=false;)
 
     
     bool NewStealthAddress(std::string& sError, std::string& sLabel, CStealthAddress& sxAddr);
